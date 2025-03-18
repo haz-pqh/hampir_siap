fe844e687ec2b621f0b7784cf801585be78c7194
haz-pqh <hazpiqah@gmail.com>
2025-03-19 03:04:56 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTxTHwLuR5Vfs8qL9iETT2vtXJB1Y9o08",
+  authDomain: "feedbacks-4c756.firebaseapp.com",
+  projectId: "feedbacks-4c756",
+  storageBucket: "feedbacks-4c756.firebasestorage.app",
+  messagingSenderId: "222330855056",
+  appId: "1:222330855056:web:09bec960d8e9b183050215"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
